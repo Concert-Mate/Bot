dcbea9b8ab2ebf80b74014341308ec40adf61f21
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-20 19:15:04 +0700

    Добавил документацию к брокерскому сервису

--- services/broker/broker.py
+++ services/broker/broker.py
@@ -14,7 +14,16 @@ class Broker(ABC):
             host: str,
             port: int
     ) -> None:
-        pass
+        """
+        Connects to broker.
+
+        :param queue_name: name of broker's queue of messages.
+        :param user_name: name of broker user
+        :param password: password of broker user
+        :param host: host of broker
+        :param port: port of broker
+        :raises BrokerException: if connection fails
+        """
 
     @abstractmethod
     async def start_listening(
@@ -22,4 +31,9 @@ class Broker(ABC):
             on_message_callback: Callable[[BrokerEvent], Coroutine[Any, Any, None]],
             on_error_callback: Callable[[Exception], Coroutine[Any, Any, None]]
     ) -> None:
-        pass
+        """
+        Thread that calls it starts listening messages from broker.
+
+        :param on_message_callback: coroutine that will be awaited when a message is received
+        :param on_error_callback: coroutine that will be awaited when an error occurs
+        """
